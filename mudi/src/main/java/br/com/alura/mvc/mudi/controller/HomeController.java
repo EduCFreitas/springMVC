0f644b3809301b5b0396e5e2261ecec8ece46753
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-11-09 15:29:56 -0300

    Aula 1.3: Usando JPA puro

--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,7 +1,10 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,16 +14,15 @@ import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Samsung Galaxy S20");
-		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/61NKqOyMPGL._AC_SL1000_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Smartphone-Samsung-Galaxy-128GB-Cinza/dp/B085TRDPZ4/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=s20&qid=1604520551&sr=8-1");
-		pedido.setDescricao("Uma descrição qualquer para esse pedido");
+		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+		List<Pedido> pedidos = query.getResultList();
 		
-		List<Pedido> pedidos = Arrays.asList(pedido);
 		model.addAttribute("pedidos", pedidos);
 		
 		return "home";
